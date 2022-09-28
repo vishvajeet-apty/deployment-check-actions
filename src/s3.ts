19a68ef0bd7856f7336e1c075fd23faf7d11b261
vishvajeet-apty <vishvajeet.singh@apty.io>
2022-09-28 20:56:42 +0530

    done-for-manual-deploy

--- src/s3.ts
+++ src/s3.ts
@@ -13,68 +13,6 @@ export const initAWS = (input: AWSConfig): void => {
   })
 }
 
-const toJSON = (input: any): any => {
-  try {
-    return JSON.parse(input)
-  } catch (e) {}
-  return undefined
-}
-// export const updateS3Object = async (
-//   Body: S3.Body,
-//   {Bucket, Key}: S3Base,
-//   branchName: string
-// ): Promise<void> => {
-//   return new Promise(async (res, rej) => {
-//     core.info('It depends on the type of event')
-//     if (eventType === 'push') {
-//       const branchArray = JSON.parse(Body.toString())
-//       branchArray.branches.push(branchName)
-//       core.info(JSON.stringify(branchArray))
-//       let params = {
-//         Bucket,
-//         Key,
-//         Body: JSON.stringify(branchArray)
-//       }
-//       let result = await createObject(params)
-//       if (result) {
-//         core.info('succesfully pushed again')
-//         return
-//       }
-//     } else {
-//       // it is a pull request
-//       core.info(Body.toString())
-
-//       const branchList = toJSON(Body.toString())
-//       core.info(`log: info: branchList: ${branchList} !`)
-//       if (branchList && branchList.branches.includes(branchName)) {
-//         core.setFailed('Cannot deploy the already deployed branches')
-//       }
-//       return
-//     }
-//   })
-// }
-// export const isS3ObjectExists = async (
-//   {Bucket, Key}: S3Base,
-//   branchName: string
-// ): Promise<boolean> => {
-//   return new Promise(res => {
-//     core.info(`Checking if this ${Bucket} with this path ${Key} exists or not`)
-//     s3.getObject(
-//       {
-//         Bucket,
-//         Key
-//       },
-//       async (err, data) => {
-//         if (err) {
-//           return res(false)
-//         } else {
-//           return res(true)
-//         }
-//       }
-//     )
-//   })
-// }
-
 export const getS3Object = async (
   {Bucket, Key}: S3Base,
   branchName: string
