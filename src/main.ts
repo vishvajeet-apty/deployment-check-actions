8737ebc67e24cd3a0e9b877b3b70094571ed7aa4
vishvajeet-apty <vishvajeet.singh@apty.io>
2022-09-30 08:15:05 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function run(): Promise<void> {
       frozenBranches = FrozenBranches.FromJsonString(
         frozenBranchData.toString()
       ).withBranch(branchName);
+      core.info(JSON.stringify(frozenBranches))
     }
 
     await createObject(
@@ -24,6 +25,8 @@ async function run(): Promise<void> {
       `assist/${deploy_environment}.json`,
       frozenBranches.toJsonString()
     );
+    const data = frozenBranches.getBranches();
+    core.info(JSON.stringify(data));
   } catch (err) {
     core.setFailed(
       `Error while checking/updating frozen branch details to S3 - ${err}`
