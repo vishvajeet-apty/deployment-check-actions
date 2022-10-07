b1ec3a9937e9a55340b266c935f93736543dc361
vishvajeet-apty <vishvajeet.singh@apty.io>
2022-10-07 10:08:14 +0530

    configurable serviceName

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import * as core from '@actions/core';
 import { getS3Object, createObject } from './s3';
 import { FrozenBranches } from './fileHandler';
 
-import { branchName, bucketName, deploy_environment } from './config';
+import { branchName, bucketName, deploy_environment,serviceName  } from './config';
 
 
 async function run(): Promise<void> {
-  const s3FileKey = `assist/${deploy_environment}.json`;
+  const s3FileKey = `${serviceName}/${deploy_environment}.json`;
   let frozenBranches;
   try {
+    if(deploy_environment !== 'production') {
+      process.exit(0);
+    }
     const frozenBranchData = await getS3Object(bucketName, s3FileKey);
     if (!frozenBranchData) {
       frozenBranches = new FrozenBranches([branchName]);
@@ -21,9 +24,10 @@ async function run(): Promise<void> {
 
     await createObject(
       bucketName,
-      `assist/${deploy_environment}.json`,
+      `${serviceName}/${deploy_environment}.json`,
       frozenBranches.toJsonString()
     );
+    core.info(JSON.stringify(frozenBranches.getBranches()));
   } catch (err) {
     core.setFailed(
       `Error while checking/updating frozen branch details to S3 - ${err}`
